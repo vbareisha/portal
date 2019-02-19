dadd403c7ac790080cca58b7d847b1c6be61f1c9
vbareisha <vbareisha@users.noreply.github.com>
2019-02-19 12:01:25 +0300

    -- Fixed gradle version;

--- src/main/java/com/bareisha/portal/core/dto/ExternalUserDto.java
+++ src/main/java/com/bareisha/portal/core/dto/ExternalUserDto.java
@@ -1,9 +1,22 @@
 package com.bareisha.portal.core.dto;
 
-import lombok.Data;
-
-@Data
 public class ExternalUserDto {
     private String userName;
     private String password;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
